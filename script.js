23de7ae9c7bc39bef68ea9558ffe10bf15218823
LishuGupta652 <lishugupta652@gmail.com>
2021-09-14 06:42:45 +0000

    files added|

---
+++ script.js
@@ -0,0 +1,15 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById('check')
+
+
+const richestPeople = [
+    'jeff bezos',
+    'Bill Gates',
+    'Warren',
+    'Benard',
+    'Carlos',
+    'Amamcio',
+    'Larry',
+    'Mark',
+    'Michael'
+]
